8c4cea9347ef0b00978724e922833a9fe4ff8f2e
AhmedSalmanRamay <salmanramay98@gmail.com>
2024-03-25 20:56:50 +0500

    first1

---
+++ pca10056/s140/ses/Output/Debug/Obj/ble-dktest1.1/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/ble-dktest1.1/ses_startup_nrf_common.o: \
+ C:\nrf_sdk\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\ses_startup_nrf_common.s
---
+++ pca10056/s140/ses/Output/Debug/Obj/ble-dktest1.1/thumb_crt0.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/ble-dktest1.1/thumb_crt0.o: \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.10c/source/thumb_crt0.s
